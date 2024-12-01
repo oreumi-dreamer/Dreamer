124f15f307fb40072e629590f5298c7d40cb7e55
김지훈 <kim@jihun.io>
2024-12-02 01:43:41 +0900

    ✨ 회원가입 API 개선: 인증 토큰을 사용하여 사용자 등록 여부 확인 및 중복 등록 처리 추가

--- src/app/api/join/route.js
+++ src/app/api/join/route.js
@@ -1,3 +1,4 @@
+import { headers } from "next/headers";
 import { initializeApp } from "firebase/app";
 import { getFirestore, setDoc, collection, doc } from "firebase/firestore";
 
@@ -16,6 +17,10 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export async function POST(request) {
+  const headersList = headers();
+  const authorization = headersList.get("Authorization");
+  const idToken = authorization.split("Bearer ")[1];
+
   try {
     // request.json()으로 body 데이터를 파싱
     const body = await request.json();
@@ -42,16 +47,33 @@ export async function POST(request) {
     // 생년월일을 Date 객체로 변환
     const birthDate = new Date(year, month - 1, day);
 
+    // 사용자가 이미 등록되어 있는지 확인
     const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
     const verifyResponse = await fetch(`${baseUrl}/api/auth/verify`, {
       method: "GET",
-      credentials: "include",
       headers: {
-        Cookie: request.headers.get("cookie"), // 쿠키 전달
+        Authorization: `Bearer ${idToken}`,
       },
     });
 
     const verifyData = await verifyResponse.json();
+
+    console.log("verifyData", verifyData);
+
+    if (verifyData.exists) {
+      return new Response(
+        JSON.stringify({
+          success: false,
+          message: "이미 등록된 사용자입니다.",
+        }),
+        {
+          status: 401,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
 
     // setDoc을 사용하여 문서 추가
     const docRef = await setDoc(doc(db, "users", verifyData.uid), {
