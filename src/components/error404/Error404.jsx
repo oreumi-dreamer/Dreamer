4effc250ac228a2ff2abdf215d5ed513535f7b64
chohee.hwang <manent556@gmail.com>
2024-12-02 10:38:44 +0900

    ✨ [add] 이전페이지로 돌아가는 btn 기능 구현

--- src/components/error404/Error404.jsx
+++ src/components/error404/Error404.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import styles from "./Error404.module.css";
 import Link from "next/link";
@@ -11,7 +13,12 @@ export default function Error404() {
         </Link>
       </h1>
       <p className={styles["page-not-found"]}>페이지를 찾을 수 없습니다.</p>
-      <button className={styles["go-back-btn"]}>이전 페이지로</button>
+      <button
+        className={styles["go-back-btn"]}
+        onClick={() => window.history.back()}
+      >
+        이전 페이지로
+      </button>
     </div>
   );
 }
