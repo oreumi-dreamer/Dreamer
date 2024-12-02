bf25dfdf3dfacc26b5535d62aea90cb031c7bdac
chohee.hwang <manent556@gmail.com>
2024-12-02 21:11:27 +0900

    :lipstick: [Add] 더보기, 모드전환 모달 CSS 추가설정

--- src/components/header/HeaderModal.jsx
+++ src/components/header/HeaderModal.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import styles from "./HeaderModal.module.css";
 
-function MoreModal() {
+function MoreModal({ setIsOpenModal }) {
   return (
     <ul className={styles["more-modal"]}>
       <li className={styles["modal-items"]}>
         <button className={styles["setting-btn"]}>계정 설정</button>
       </li>
       <li className={styles["modal-items"]}>
-        <button className={styles["change-mode-btn"]}>모드 전환</button>
+        <button
+          className={`${styles["change-mode-btn"]} ${styles["light-mode"]}`}
+          onClick={() => setIsOpenModal("모드 전환")}
+        >
+          모드 전환
+        </button>
       </li>
       <li className={styles["modal-items"]}>
         <button className={styles["inquiry-btn"]}>문의 사항</button>
@@ -20,6 +25,28 @@ function MoreModal() {
   );
 }
 
+function ChangeModeModal({ setIsOpenModal }) {
+  return (
+    <ul className={styles["change-mode-modal"]}>
+      <li className={styles["modal-items"]}>
+        <button
+          className={styles["back-more-modal-btn"]}
+          onClick={() => setIsOpenModal("더보기")}
+        >
+          모드 전환
+        </button>
+      </li>
+      <li className={styles["modal-items"]}>
+        <button className={styles["dark-mode-btn"]}>다크 모드</button>
+      </li>
+      <li className={styles["modal-items"]}>
+        <button className={styles["light-mode-btn"]}>라이트 모드</button>
+      </li>
+      <li className={styles["modal-items"]}>
+        <button className={styles["device-setting-btn"]}>기기 설정 사용</button>
+      </li>
+    </ul>
+  );
+}
 
-
-export { MoreModal};
+export { MoreModal, ChangeModeModal };
