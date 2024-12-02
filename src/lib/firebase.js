10beb4cb0a07b32271c85d3d1ef2df4fab45b448
김지훈 <kim@jihun.io>
2024-12-02 12:29:49 +0900

    ✨ Firebase 스토리지 초기화 추가

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -7,6 +7,7 @@ import {
 } from "firebase/auth";
 
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -22,6 +23,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 const googleProvider = new GoogleAuthProvider();
 
-export { auth, googleProvider, db };
+export { auth, googleProvider, db, storage };
