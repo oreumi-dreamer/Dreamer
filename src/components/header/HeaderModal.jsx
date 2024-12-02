d95579f5961b6424a9f87680352a69e6a5f83c73
chohee.hwang <manent556@gmail.com>
2024-12-02 15:50:25 +0900

    :memo: [Add] Header Modal MarkUp

--- src/components/header/HeaderModal.jsx
+++ src/components/header/HeaderModal.jsx
@@ -3,23 +3,39 @@ import styles from "./HeaderModal.module.css";
 
 function MoreModal() {
   return (
-    <div className={styles["modal-wrap"]}>
-      <button className={styles["setting-btn"]}>계정 설정</button>
-      <button className={styles["change-mode-btn"]}>모드 전환</button>
-      <button className={styles["inquiry-btn"]}>문의 사항</button>
-      <button className={styles["logout-btn"]}>로그아웃</button>
-    </div>
+    <ul className={styles["more-modal"]}>
+      <li className={styles["modal-items"]}>
+        <button className={styles["setting-btn"]}>계정 설정</button>
+      </li>
+      <li className={styles["modal-items"]}>
+        <button className={styles["change-mode-btn"]}>모드 전환</button>
+      </li>
+      <li className={styles["modal-items"]}>
+        <button className={styles["inquiry-btn"]}>문의 사항</button>
+      </li>
+      <li className={styles["modal-items"]}>
+        <button className={styles["logout-btn"]}>로그아웃</button>
+      </li>
+    </ul>
   );
 }
 
 function ChangeModeModal() {
   return (
-    <div className={styles["modal-wrap"]}>
-      <button>모드 전환</button>
-      <button>다크 모드</button>
-      <button>라이트 모드</button>
-      <button>기기 설정 사용</button>
-    </div>
+    <ul className={styles["change-mode-modal"]}>
+      <li className={styles["modal-items"]}>
+        <button>모드 전환</button>
+      </li>
+      <li className={styles["modal-items"]}>
+        <button>다크 모드</button>
+      </li>
+        <li className={styles["modal-items"]}>
+        <button>라이트 모드</button>
+      </li>
+      <li className={styles["modal-items"]}>
+        <button>기기 설정 사용</button>
+      </li>
+    </ul>
   );
 }
 
