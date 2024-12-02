e7dbd0e58269e25b283661c77dc31425ea682fcd
chohee.hwang <manent556@gmail.com>
2024-12-02 13:36:01 +0900

    :memo: [fix] Header btn ClassName 수정

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+"use client";
+
+import React, { isValidElement, useRef, useState } from "react";
 import styles from "./Header.module.css";
 import Link from "next/link";
-import {MoreModal, ChangeModeModal} from "./HeaderModal";
+import { MoreModal, ChangeModeModal } from "./HeaderModal";
 
 export default function Header() {
+  const buttonRef = useRef(null);
+  const [isActive, setIsActive] = useState("홈");
+
+  const handleButtonClick = (e) => {
+    const buttons = document.querySelectorAll("button");
+    const clickedBtn = e.currentTarget;
+
+    setIsActive(clickedBtn.textContent);
+
+    buttons.forEach((button) => {
+      if (button !== isActive) {
+        button.classList.remove(styles.active);
+      }
+    });
+    clickedBtn.classList.add(styles.active);
+  };
+
   return (
     <header className={styles.header}>
       <h1 className={styles.logo}>
@@ -18,28 +37,63 @@ export default function Header() {
         <nav>
           <ul className={styles.nav}>
             <li className={styles["nav-item"]}>
-              <button className={`${styles.home} ${styles.active}`}>홈</button>
+              <button
+                className={`${styles["home-btn"]} ${styles.active}`}
+                onClick={handleButtonClick}
+              >
+                홈
+              </button>
             </li>
             <li className={styles["nav-item"]}>
-              <button className={styles.search}>검색</button>
+              <button
+                className={styles["search-btn"]}
+                onClick={handleButtonClick}
+              >
+                검색
+              </button>
             </li>
             <li className={styles["nav-item"]}>
-              <button className={styles.message}>메시지</button>
+              <button
+                className={styles["message-btn"]}
+                onClick={handleButtonClick}
+              >
+                메시지
+              </button>
             </li>
             <li className={styles["nav-item"]}>
-              <button className={styles.alarm}>알림</button>
+              <button
+                className={styles["alarm-btn"]}
+                onClick={handleButtonClick}
+              >
+                알림
+              </button>
             </li>
             <li className={styles["nav-item"]}>
-              <button className={styles.wright}>글쓰기</button>
+              <button
+                className={styles["writing-btn"]}
+                onClick={handleButtonClick}
+              >
+                글쓰기
+              </button>
             </li>
           </ul>
         </nav>
-        <button href={"#"} className={styles["profile-btn"]}>
-          <img src="/images/basic-profile.svg" alt="프로필사진" />
+        <button className={styles["profile-btn"]} onClick={handleButtonClick}>
+          <img
+            src="/images/basic-profile.svg"
+            alt="프로필사진"
+            loading="lazy"
+          />
           <p>JINI</p>
         </button>
-        <button className={styles.more}>더보기</button>
-        <MoreModal/>
+        <button
+          className={styles["more-btn"]}
+          ref={buttonRef}
+          onClick={handleButtonClick}
+        >
+          더보기
+        </button>
+        <MoreModal />
       </div>
     </header>
   );
