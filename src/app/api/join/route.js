c6317314788b2740ea39f0bfe4ba5d3f49b612d2
김지훈 <kim@jihun.io>
2024-12-02 12:19:48 +0900

    🎨 Firebase 초기화 방식 변경

--- src/app/api/join/route.js
+++ src/app/api/join/route.js
@@ -1,20 +1,6 @@
 import { headers } from "next/headers";
-import { initializeApp } from "firebase/app";
-import { getFirestore, setDoc, collection, doc } from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-};
-
-// Firebase 앱 초기화
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+import { db } from "@/lib/firebase"; // Firebase 초기화된 인스턴스 import
+import { setDoc, collection, doc } from "firebase/firestore";
 
 export async function POST(request) {
   const headersList = headers();
