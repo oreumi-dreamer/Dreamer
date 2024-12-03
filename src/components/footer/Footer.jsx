948f692be5a94bd840f7372bd6ce11f027dea6a9
chohee.hwang <manent556@gmail.com>
2024-12-03 14:58:38 +0900

    :memo: [Fix] Footer ClassName 수정

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <input type="text" className={styles["footer-search"]} />
+      <input type="text" className={styles["footer-search-inp"]} />
       <article className={styles["recommend-wrap"]}>
         <p className={styles["recommend-title"]}>추천 DREAMER</p>
         <div className={styles["recommend-list"]}></div>
