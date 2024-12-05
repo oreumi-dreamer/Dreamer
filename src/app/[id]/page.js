a3d19df500e037207c2f16adeb986d8422eb5d3e
김지훈 <kim@jihun.io>
2024-12-06 01:19:14 +0900

    ✨ 사용자 프로필 페이지에 Header, Profile, Footer 컴포넌트 추가

--- src/app/[id]/page.js
+++ src/app/[id]/page.js
@@ -1,5 +1,16 @@
 // 사용자 프로필 페이지
 
+import Header from "@/components/header/Header";
+import Footer from "@/components/footer/Footer";
+import Profile from "@/components/profile/Profile";
+import styles from "@/app/page.module.css";
+
 export default function UserProfile(props) {
-  return <p>{props.params.id}</p>;
+  return (
+    <div className={styles.container}>
+      <Header />
+      <Profile userName={props.params.id} />
+      <Footer />
+    </div>
+  );
 }
