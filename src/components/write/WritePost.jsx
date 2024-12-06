41d479d0a93b1f1eb4c7c1a37e1364b95395bbdd
han.j33 <jeehyun0212@naver.com>
2024-12-06 17:04:12 +0900

    📝 글작성 모달(WritePost) 마크업 수정(submit 버튼 추가)

--- src/components/write/WritePost.jsx
+++ src/components/write/WritePost.jsx
@@ -12,82 +12,100 @@ export default function WritePost() {
   return (
     <dialog className={styles["new-post"]} open>
       <h2 className="sr-only">새로운 글 작성</h2>
-      <Image src="/images/rabbit.svg" width={49} height={49} />
+      <Image src="/images/rabbit.svg" width={52} height={52} />
       <p>
         {"JINI"}
         <p>@jini</p>
       </p>
 
       <button className={styles["btn-close"]}>
-        <Image src="/images/close.svg" width={22} height={22}></Image>
+        <Image src="/images/close.svg" width={40} height={40}></Image>
         <span className="sr-only">닫기</span>
       </button>
 
-      <label id={styles["title"]}>
-        Title
-        <input type="text" for="title" placeholder="00년 00월 00일 꿈 일기" />
-      </label>
-      <label id="hidden">
-        <input type="checkbox" for="hidden" />
-        비공개
-      </label>
-
-      <div className={styles["write-field"]}>
-        <div className={styles["write-field-opt"]}>
-          {/* 버튼 누르면 해시태그 입력용 모달 나타나기? */}
-          <button>
-            <Image src="/images/plus-circle.svg" width={28} height={28} />
-            <span className="sr-only">태그 추가하기</span>
-          </button>
-          {/* 기분 추가 */}
-          <p>
-            지금 상태
-            <button>{/* 누르면 체크박스 모달 열기 */}</button>
-          </p>
-
-          <p>오늘의 꿈 별점: </p>
-          <div className="rate-stars">
+      <form id={styles["new-post-form"]}>
+        <div id={styles["title"]}>
+          <label id={styles["title-input"]}>
+            Title
             <input
-              type="radio"
-              className={styles["rate-star"]}
-              name="rate-star"
-              value={1}
+              type="text"
+              for="title"
+              placeholder={`00년 00월 00일 꿈 일기`}
             />
-            <input
-              type="radio"
-              className={styles["rate-star"]}
-              name="rate-star"
-              value={2}
-            />
-            <input
-              type="radio"
-              className={styles["rate-star"]}
-              name="rate-star"
-              value={3}
-            />
-            <input
-              type="radio"
-              className={styles["rate-star"]}
-              name="rate-star"
-              value={4}
-            />
-            <input
-              type="radio"
-              className={styles["rate-star"]}
-              name="rate-star"
-              value={5}
-            />
-          </div>
+          </label>
+          <label id={styles["hidden"]}>
+            <input type="checkbox" for="hidden" />
+            비공개
+          </label>
         </div>
 
-        <label>
-          <span className="sr-only">글 작성</span>
-          <textarea
-            placeholder="오늘은 어떤 꿈을 꾸셨나요?"
-            className={styles["text-field-area"]}
-          />
-        </label>
-      </div>
+        <div className={styles["write-field"]}>
+          <div className={styles["write-field-opt"]}>
+            {/* 버튼 누르면 해시태그 입력용 모달 나타나기? */}
+            <button>
+              <Image src="/images/plus-circle.svg" width={28} height={28} />
+              <span className="sr-only">태그 추가하기</span>
+            </button>
+            {/* 기분 추가 */}
+            <p>
+              지금 상태
+              <button>{/* 누르면 체크박스 모달 열기 */}</button>
+            </p>
+
+            <p>오늘의 꿈 별점: </p>
+            <div className="rate-stars">
+              <input
+                type="radio"
+                className={styles["rate-star"]}
+                name="rate-star"
+                value={1}
+              />
+              <input
+                type="radio"
+                className={styles["rate-star"]}
+                name="rate-star"
+                value={2}
+              />
+              <input
+                type="radio"
+                className={styles["rate-star"]}
+                name="rate-star"
+                value={3}
+              />
+              <input
+                type="radio"
+                className={styles["rate-star"]}
+                name="rate-star"
+                value={4}
+              />
+              <input
+                type="radio"
+                className={styles["rate-star"]}
+                name="rate-star"
+                value={5}
+              />
+            </div>
+          </div>
+
+          {/* 구분선 */}
+          <span className={styles["break-line"]}></span>
+
+          <label>
+            <span className="sr-only">글 작성</span>
+            <textarea
+              placeholder="오늘은 어떤 꿈을 꾸셨나요?"
+              className={styles["text-field-area"]}
+            />
+          </label>
+        </div>
+      </form>
+      <button
+        type="submit"
+        form="new-post-form"
+        className={styles["btn-submit"]}
+      >
+        전송
+      </button>
     </dialog>
   );
 }
