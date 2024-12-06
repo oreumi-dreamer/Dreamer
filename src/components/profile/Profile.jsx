c88112e667134c05694a5c643f7bd1652e3d6635
김지훈 <kim@jihun.io>
2024-12-06 12:30:06 +0900

    ✨ 사용자 프로필 페이지에 팔로우/언팔로우 기능 추가

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -10,6 +10,30 @@ export default function Profile({ userName }) {
   const [posts, setPosts] = useState(null);
   const [loading, setLoading] = useState(true);
 
+  const changeFollow = () => {
+    setProfile((currentProfile) => ({
+      ...currentProfile,
+      isFollowing: !currentProfile.isFollowing,
+      followersCount: currentProfile.isFollowing
+        ? currentProfile.followersCount - 1
+        : currentProfile.followersCount + 1,
+    }));
+  };
+
+  const toggleFollow = async () => {
+    changeFollow();
+
+    try {
+      const res = await fetchWithAuth(`/api/account/follow/${profile.id}`);
+      if (!res.ok || res.status !== 200) {
+        throw new Error("팔로우 실패");
+      }
+    } catch (error) {
+      console.error("Error toggling follow:", error);
+      changeFollow();
+    }
+  };
+
   useEffect(() => {
     const getProfile = async () => {
       const res = await fetchWithAuth(
@@ -26,6 +50,8 @@ export default function Profile({ userName }) {
           bio: data.bio,
           length: data.length,
           profileImageUrl: data.profileImageUrl,
+          followersCount: data.followersCount,
+          followingCount: data.followingCount,
         });
       }
 
@@ -66,17 +92,29 @@ export default function Profile({ userName }) {
                   <div className={styles["profile-name"]}>{profile.name}</div>
                   <div className={styles["profile-id"]}>@{profile.id}</div>
                 </div>
-                <button className={`${styles["profile-btn"]} ${styles.active}`}>
-                  팔로잉
-                </button>
+                {profile.isFollowing ? (
+                  <button
+                    onClick={toggleFollow}
+                    className={`${styles["profile-btn"]} ${styles.active}`}
+                  >
+                    팔로잉
+                  </button>
+                ) : (
+                  <button
+                    onClick={toggleFollow}
+                    className={`${styles["profile-btn"]}`}
+                  >
+                    팔로우
+                  </button>
+                )}
               </div>
               <dl className={styles["profile-stat"]}>
                 <dt>게시물</dt>
                 <dd>{profile.length}개</dd>
                 <dt>팔로우</dt>
-                <dd>0명</dd>
+                <dd>{profile.followersCount}명</dd>
                 <dt>팔로워</dt>
-                <dd>999명</dd>
+                <dd>{profile.followingCount}명</dd>
               </dl>
               <div className={styles["profile-bio"]}>{profile.bio}</div>
             </div>
