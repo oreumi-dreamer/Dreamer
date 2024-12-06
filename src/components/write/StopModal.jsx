ccc0a0256eb9d7b0e763f275ed7058f510af3861
han.j33 <jeehyun0212@naver.com>
2024-12-06 11:39:59 +0900

    📝 작성중단 모달(StopModal) 마크업 작성

--- src/components/write/StopModal.jsx
+++ src/components/write/StopModal.jsx
@@ -1,17 +1,16 @@
 import React from "react";
+import styles from "./StopModal.module.css";
 
 export default function StopModal(props) {
-  function closeModal() {
-    props.closeModal();
-  }
-
   return (
     <>
-      <dialog>
+      <dialog className={styles["stop-modal"]} open>
         <p>작성 중인 글이 있습니다. 창을 닫으시겠습니까?</p>
-        <button onClick={closeModal}>네</button>
-        <button>아니오</button>
-        <button>
+        <div>
+          <button className={styles["btn-yes"]}>네</button>
+          <button className={styles["btn-no"]}>아니오</button>
+        </div>
+        <button className={styles["btn-close"]}>
           <span className="sr-only">닫기</span>
         </button>
       </dialog>
