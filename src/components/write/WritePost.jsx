0fe7533ceae69918c94766c5c239285b687f589c
han.j33 <jeehyun0212@naver.com>
2024-12-06 13:33:56 +0900

    📝  글작성 모달(WritePost) 마크업 수정

--- src/components/write/WritePost.jsx
+++ src/components/write/WritePost.jsx
@@ -3,25 +3,11 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "./WritePost.module.css";
 import StopModal from "./StopModal";
+import HashtagModal from "./HashtagModal";
 
 export default function WritePost() {
   // 작성 중단 상태 저장(영역 밖 클릭 여부)
   const [isStop, setIsStop] = useState(false);
-
-  // 별점: 마우스 hover 상태 저장
-  const [isHover, setIsHover] = useState(false);
-  const handleMouseOver = () => {
-    setIsHover(true);
-  };
-  const handleMouseOut = () => {
-    setIsHover(false);
-  };
-
-  // 별점: 마우스 클릭 이벤트 저장
-  const [isClick, setIsClick] = useState(false);
-  const handleClick = () => {
-    setIsClick(true);
-  };
 
   return (
     <dialog className={styles["new-post"]} open>
@@ -37,7 +23,7 @@ export default function WritePost() {
         <span className="sr-only">닫기</span>
       </button>
 
-      <label id="title">
+      <label id={styles["title"]}>
         Title
         <input type="text" for="title" placeholder="00년 00월 00일 꿈 일기" />
       </label>
@@ -47,49 +33,51 @@ export default function WritePost() {
       </label>
 
       <div className={styles["write-field"]}>
-        {/* 버튼 누르면 해시태그 입력용 모달 나타나기? */}
-        <button>
-          <Image src="/images/plus-circle.svg" width={28} height={28} />
-          <span className="sr-only">태그 추가하기</span>
-        </button>
-        {/* 기분 추가 */}
-        <p>
-          지금 상태
-          <button>{/* 누르면 체크박스 모달 열기 */}</button>
-        </p>
+        <div className={styles["write-field-opt"]}>
+          {/* 버튼 누르면 해시태그 입력용 모달 나타나기? */}
+          <button>
+            <Image src="/images/plus-circle.svg" width={28} height={28} />
+            <span className="sr-only">태그 추가하기</span>
+          </button>
+          {/* 기분 추가 */}
+          <p>
+            지금 상태
+            <button>{/* 누르면 체크박스 모달 열기 */}</button>
+          </p>
 
-        <p>오늘의 꿈 별점: </p>
-        <div className="rate-stars">
-          <input
-            type="radio"
-            className={styles["rate-star"]}
-            name="rate-star"
-            value={1}
-          />
-          <input
-            type="radio"
-            className={styles["rate-star"]}
-            name="rate-star"
-            value={2}
-          />
-          <input
-            type="radio"
-            className={styles["rate-star"]}
-            name="rate-star"
-            value={3}
-          />
-          <input
-            type="radio"
-            className={styles["rate-star"]}
-            name="rate-star"
-            value={4}
-          />
-          <input
-            type="radio"
-            className={styles["rate-star"]}
-            name="rate-star"
-            value={5}
-          />
+          <p>오늘의 꿈 별점: </p>
+          <div className="rate-stars">
+            <input
+              type="radio"
+              className={styles["rate-star"]}
+              name="rate-star"
+              value={1}
+            />
+            <input
+              type="radio"
+              className={styles["rate-star"]}
+              name="rate-star"
+              value={2}
+            />
+            <input
+              type="radio"
+              className={styles["rate-star"]}
+              name="rate-star"
+              value={3}
+            />
+            <input
+              type="radio"
+              className={styles["rate-star"]}
+              name="rate-star"
+              value={4}
+            />
+            <input
+              type="radio"
+              className={styles["rate-star"]}
+              name="rate-star"
+              value={5}
+            />
+          </div>
         </div>
 
         <label>
