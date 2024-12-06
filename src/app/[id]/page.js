1b36e87d9354070a590da54580c9327bfad4c5b5
김지훈 <kim@jihun.io>
2024-12-06 10:47:07 +0900

    ✨ 사용자 프로필 페이지에 메타데이터 생성 함수 추가

--- src/app/[id]/page.js
+++ src/app/[id]/page.js
@@ -4,6 +4,11 @@ import Header from "@/components/header/Header";
 import Footer from "@/components/footer/Footer";
 import Profile from "@/components/profile/Profile";
 import styles from "@/app/page.module.css";
+import { metadataMaker } from "@/utils/metadata";
+
+export async function generateMetadata({ params }) {
+  return metadataMaker(`${params.id}: DREAMER`, "당신의 꿈을 들려주세요!");
+}
 
 export default function UserProfile(props) {
   return (
