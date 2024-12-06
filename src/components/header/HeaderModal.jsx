23dc85c8c5a891d50497df2d7f6359ac9dad4390
chohee.hwang <manent556@gmail.com>
2024-12-06 10:03:59 +0900

    :memo: [Fix] HeaderModal 역할에따른 태그 및 클래스명 변경

--- src/components/header/HeaderModal.jsx
+++ src/components/header/HeaderModal.jsx
@@ -1,73 +1,91 @@
 import React, { useEffect, useRef } from "react";
 import styles from "./HeaderModal.module.css";
 import { outsideClickModalClose } from "@/utils/outsideClickModalClose";
+import { openModal, closeModal, setModalType } from "@/store/modalSlice";
 import Link from "next/link";
+import { useDispatch, useSelector } from "react-redux";
 
-function MoreModal({ setOpenModalName, isOpen, onClose }) {
+const HeaderBaseModal = ({ children }) => {
   const modalRef = useRef(null);
+  const dispatch = useDispatch();
+  const { isOpen, modalType } = useSelector((state) => state.modal);
 
   useEffect(() => {
     if (isOpen) {
-      outsideClickModalClose(modalRef, onClose);
+      outsideClickModalClose(modalRef, () => dispatch(closeModal()));
     }
-  }, [isOpen, onClose]);
+  }, [isOpen, dispatch]);
+
+  if (!isOpen) return null;
 
   return (
-    <ul className={styles["more-modal"]} ref={modalRef}>
-      <li className={styles["modal-items"]}>
-        <Link href="/account" className={styles["setting-btn"]}>
-          계정 설정
-        </Link>
-      </li>
-      <li className={styles["modal-items"]}>
-        <button
-          className={`${styles["change-mode-btn"]} ${styles["light-mode"]}`}
-          onClick={() => setOpenModalName("모드 전환")}
-        >
-          모드 전환
-        </button>
-      </li>
-      <li className={styles["modal-items"]}>
-        <button className={styles["inquiry-btn"]}>문의 사항</button>
-      </li>
-      <li className={styles["modal-items"]}>
-        <Link href="/logout">
-          <button className={styles["logout-btn"]}>로그아웃</button>
-        </Link>
-      </li>
-    </ul>
+    <div className={styles["modal-wrapper"]} ref={modalRef}>
+      {/* {modalType === "더보기" &&
+       <MoreModal />
+       } */}
+      {/* {modalType === "모드 전환" && 
+      // <ChangeModeModal />
+      } */}
+    </div>
+  );
+};
+
+function MoreModal() {
+  return (
+    <HeaderBaseModal>
+      <ul className={styles["more-modal"]}>
+        <li className={styles["modal-items"]}>
+          <Link href="/account" className={`${styles["setting-btn"]} ${styles["more-modal-btn"]}`}>
+            계정 설정
+          </Link>
+        </li>
+        <li className={styles["modal-items"]}>
+          <button
+            className={`${styles["change-mode-btn"]} ${styles["more-modal-btn"]} ${styles["light-mode"]}`}
+            onClick={() => setModalType("모드 전환")}
+          >
+            모드 전환
+          </button>
+        </li>
+        <li className={`${styles["modal-items"]} ${styles["more-modal-btn"]}`}>
+          <button className={styles["inquiry-btn"]}>문의 사항</button>
+        </li>
+        <li className={styles["modal-items"]}>
+          <Link href="/logout" className={`${styles["logout-btn"]} ${styles["more-modal-btn"]}`}>
+            로그아웃
+          </Link>
+        </li>
+      </ul>
+    </HeaderBaseModal>
   );
 }
 
-function ChangeModeModal({ setOpenModalName, isOpen, onClose }) {
-  const modalRef = useRef(null);
-
-  useEffect(() => {
-    if (isOpen) {
-      outsideClickModalClose(modalRef, onClose);
-    }
-  }, [isOpen, onClose]);
+function ChangeModeModal() {
   return (
-    <ul className={styles["change-mode-modal"]} ref={modalRef}>
-      <li className={styles["modal-items"]}>
-        <button
-          className={styles["back-more-modal-btn"]}
-          onClick={() => setOpenModalName("더보기")}
-        >
-          모드 전환
-        </button>
-      </li>
-      <li className={styles["modal-items"]}>
-        <button className={styles["dark-mode-btn"]}>다크 모드</button>
-      </li>
-      <li className={styles["modal-items"]}>
-        <button className={styles["light-mode-btn"]}>라이트 모드</button>
-      </li>
-      <li className={styles["modal-items"]}>
-        <button className={styles["device-setting-btn"]}>기기 설정 사용</button>
-      </li>
-    </ul>
+    <HeaderBaseModal>
+      <ul className={styles["change-mode-modal"]}>
+        <li className={styles["modal-items"]}>
+          <button
+            className={styles["back-more-modal-btn"]}
+            onClick={() => setModalType("모드 전환")}
+          >
+            모드 전환
+          </button>
+        </li>
+        <li className={styles["modal-items"]}>
+          <button className={styles["dark-mode-btn"]}>다크 모드</button>
+        </li>
+        <li className={styles["modal-items"]}>
+          <button className={styles["light-mode-btn"]}>라이트 모드</button>
+        </li>
+        <li className={styles["modal-items"]}>
+          <button className={styles["device-setting-btn"]}>
+            기기 설정 사용
+          </button>
+        </li>
+      </ul>
+    </HeaderBaseModal>
   );
 }
 
-export { MoreModal, ChangeModeModal };
+export { HeaderBaseModal };
