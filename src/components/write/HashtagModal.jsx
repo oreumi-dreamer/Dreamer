0058c3226927848e751b64ef74cd1b4072b0ac94
han.j33 <jeehyun0212@naver.com>
2024-12-09 14:20:03 +0900

    📝 선택지 모달(HashtagModal) 마크업 수정

--- src/components/write/HashtagModal.jsx
+++ src/components/write/HashtagModal.jsx
@@ -4,20 +4,16 @@ import styles from "./HashtagModal.module.css";
 export default function HashtagModal() {
   return (
     <dialog className={styles["podal-on-writepost"]} open>
-      {/* 추후 선택지 데이터 불러오기 -- 시작 */}
-      <label>
-        <input type="checkbox" className={styles["hashtag-picker"]} />
-        선택지
-      </label>
-      <label>
-        <input type="checkbox" className={styles["hashtag-picker"]} />
-        선택지
-      </label>
-      <label>
-        <input type="checkbox" className={styles["hashtag-picker"]} />
-        선택지
-      </label>
-      {/* 추후 선택지 데이터 불러오기 -- 끝 */}
+      <div className={styles["hashtag-picker-container"]}>
+        {/* 추후 선택지 데이터 불러오기 -- 시작 */}
+        <p>
+          <input type="checkbox" className={styles["hashtag-picker"]} id="1" />
+          <label for="1">선택지</label>
+        </p>
+        {/* 추후 선택지 데이터 불러오기 -- 끝 */}
+      </div>
+      <p className={styles["sub-text"]}>(최대 5개 선택 가능)</p>
+      <button>확인</button>
     </dialog>
   );
 }
