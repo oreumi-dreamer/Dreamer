c99d716724d62ef2f3954b37029815fe815639b1
chohee.hwang <manent556@gmail.com>
2024-12-09 13:16:53 +0900

    :lipstick: [Fix] 모달 위치조정

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,12 @@
 "use client";
 
-import React, {useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import WideHeader from "@/components/header/WideHeader";
 import { openModal, closeModal, setModalType } from "@/store/modalSlice";
 import { useDispatch, useSelector } from "react-redux";
 export default function Header() {
   const buttonRef = useRef(null);
+  const [modalStyle, setModalStyle] = useState({});
   const [isActive, setIsActive] = useState("홈");
   const { isOpen, modalType } = useSelector((state) => state.modal);
 
@@ -24,37 +25,33 @@ export default function Header() {
     setIsActive(btn);
   };
 
-  // const handleCloseModal = (e) => {
-  //   setIsOpenModal(false);
-  // };
+  const updateModalPosition = () => {
+    if (buttonRef.current) {
+      const buttonRect = buttonRef.current.getBoundingClientRect();
+      setModalStyle({
+        position: "absolute",
+        top: `${buttonRect.top + window.scrollY - 600}px`,
+        left: `${buttonRect.left + window.scrollX - 80}px`,
+      });
+    }
+  };
 
-  // // 더보기 버튼의 위치에 따른 모달의 유동적움직임함수
-  // const updateModalPosition = () => {
-  //   if (buttonRef.current) {
-  //     const buttonRect = buttonRef.current.getBoundingClientRect();
-  //     setModalStyle({
-  //       position: "absolute",
-  //       top: `${buttonRect.top + window.scrollY + -600}px`,
-  //       left: `${buttonRect.left + window.scrollX + -80}px`,
-  //     });
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   if (modalType) {
-  //     updateModalPosition();
-  //     window.addEventListener("resize", updateModalPosition);
-  //   }
-  //   return () => {
-  //     window.removeEventListener("resize", updateModalPosition);
-  //   };
-  // }, [modalType]);
+  useEffect(() => {
+    if (isOpen) {
+      updateModalPosition();
+      window.addEventListener("resize", updateModalPosition);
+    }
+    return () => {
+      window.removeEventListener("resize", updateModalPosition);
+    };
+  }, [isOpen, modalType]);
 
   return (
     <>
       <WideHeader
         onMoreBtnClick={() => handleMoreBtnClick("moreModal")}
         buttonRef={buttonRef}
+        modalStyle={modalStyle}
         isActive={isActive}
         handleActiveBtn={handleActiveBtn}
       />
