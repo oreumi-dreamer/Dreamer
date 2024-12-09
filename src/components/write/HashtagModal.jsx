0b6bbcea4701402fb6288fb8c732acebe1cc6bec
han.j33 <jeehyun0212@naver.com>
2024-12-09 19:01:42 +0900

    ✨ 글작성 모달(WritePost)에 선택지 모달 오픈 기능 추가

--- src/components/write/HashtagModal.jsx
+++ src/components/write/HashtagModal.jsx
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./HashtagModal.module.css";
 
-export default function HashtagModal() {
+export default function HashtagModal({
+  isModalOpen,
+  closeModal,
+  selectedItems,
+}) {
+  const dialogRef = useRef(null);
+  useEffect(() => {
+    if (isModalOpen && dialogRef.current) {
+      dialogRef.current.showModal();
+    } else if (dialogRef.current) {
+      dialogRef.current.close();
+    }
+  }, [isModalOpen]);
+  const handleBackgroundClick = (e) => {
+    if (e.target === e.currentTarget) closeModal();
+  };
+
   return (
-    <dialog className={styles["podal-on-writepost"]} open>
-      <div className={styles["hashtag-picker-container"]}>
+    <dialog
+      ref={dialogRef}
+      onClick={handleBackgroundClick}
+      className={styles["modal-on-writepost"]}
+    >
+      <form className={styles["hashtag-picker-container"]}>
         {/* 추후 선택지 데이터 불러오기 -- 시작 */}
         <p>
           <input type="checkbox" className={styles["hashtag-picker"]} id="1" />
           <label for="1">선택지</label>
         </p>
         {/* 추후 선택지 데이터 불러오기 -- 끝 */}
-      </div>
+      </form>
       <p className={styles["sub-text-container"]}>
         <span className={styles["sub-text"]}>(최대 5개 선택 가능)</span>
-        <button>확인</button>
+        <button onClick={closeModal}>확인</button>
       </p>
     </dialog>
   );
