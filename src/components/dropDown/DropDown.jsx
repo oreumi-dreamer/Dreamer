e67ea3d7fd9e1cf488756d30901a4ba9ba163311
chohee.hwang <manent556@gmail.com>
2024-12-10 23:05:02 +0900

    :memo: [Fix] 드롭다운 컴포넌트 수정

--- src/components/dropDown/DropDown.jsx
+++ src/components/dropDown/DropDown.jsx
@@ -2,56 +2,59 @@ import React from "react";
 import { useSelector } from "react-redux";
 import styles from "./DropDown.module.css";
 
-export default function DropDown() {
-  const { isOpen, modalType } = useSelector((state) => state.modal);
+function DropDown() {
   return (
     <>
-    <div className={styles["drop-down"]}>
-      <MyPost />
-    </div>
-    <div className={styles["drop-down"]}>
-      <OtherPost />
-    </div>
+      <div className={styles["drop-down"]}>
+        {isOpen && modalType === "isMyPost" && <MyPost />}
+        {isOpen && modalType === "isNotMyPost" && <OtherPost />}
+      </div>
     </>
   );
 }
 
-function MyPost() {
+export function MyPost() {
   return (
-    <ul className={styles["my-post"]}>
-      <li className={styles["drop-down-items"]}>
-        <button className={`${styles["edit-btn"]} ${styles["drop-down-item"]}`}>
-          수정하기
-        </button>
-      </li>
-      <li className={styles["drop-down-items"]}>
-        <button
-          className={`${styles["delete-btn"]} ${styles["drop-down-item"]}`}
-        >
-          삭제하기
-        </button>
-      </li>
-      <li className={styles["drop-down-items"]}>
-        <button
-          className={`${styles["secret-btn"]} ${styles["drop-down-item"]}`}
-        >
-          비밀글로 변경하기
-        </button>
-      </li>
-    </ul>
+    <div className={styles["drop-down"]}>
+      <ul className={styles["my-post"]}>
+        <li className={styles["drop-down-items"]}>
+          <button
+            className={`${styles["edit-btn"]} ${styles["drop-down-item"]}`}
+          >
+            수정하기
+          </button>
+        </li>
+        <li className={styles["drop-down-items"]}>
+          <button
+            className={`${styles["delete-btn"]} ${styles["drop-down-item"]}`}
+          >
+            삭제하기
+          </button>
+        </li>
+        <li className={styles["drop-down-items"]}>
+          <button
+            className={`${styles["secret-btn"]} ${styles["drop-down-item"]}`}
+          >
+            비밀글로 변경하기
+          </button>
+        </li>
+      </ul>
+    </div>
   );
 }
 
-function OtherPost() {
+export function OtherPost() {
   return (
-    <ul className={styles["other-post"]}>
-      <li className={styles["drop-down-items"]}>
-        <button
-          className={`${styles["report-btn"]} ${styles["drop-down-item"]}`}
-        >
-          신고하기
-        </button>
-      </li>
-    </ul>
+    <div className={styles["drop-down"]}>
+      <ul className={styles["other-post"]}>
+        <li className={styles["drop-down-items"]}>
+          <button
+            className={`${styles["report-btn"]} ${styles["drop-down-item"]}`}
+          >
+            신고하기
+          </button>
+        </li>
+      </ul>
+    </div>
   );
 }
