543ff71015a17f6e1cd923b8775a83068dcb34b2
han.j33 <jeehyun0212@naver.com>
2024-12-10 15:01:17 +0900

    🎨 작성 중단 모달(StopModal) 수정(모달 열기)

--- src/components/write/StopModal.jsx
+++ src/components/write/StopModal.jsx
@@ -1,23 +1,30 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./StopModal.module.css";
 
-export default function StopModal({ closeModal, onConfirm }) {
+export default function StopModal({ isStopModalOpen, closeModal, onConfirm }) {
+  const dialogRef = useRef(null);
+  useEffect(() => {
+    if (isStopModalOpen && dialogRef.current) {
+      dialogRef.current.showModal();
+    } else if (dialogRef.current) {
+      dialogRef.current.close();
+    }
+  }, [isStopModalOpen]);
+
   return (
-    <>
-      <dialog className={styles["stop-modal"]} open>
-        <p>작성 중인 글이 있습니다. 창을 닫으시겠습니까?</p>
-        <div>
-          <button onClick={onConfirm} className={styles["btn-yes"]}>
-            네
-          </button>
-          <button onClick={closeModal} className={styles["btn-no"]}>
-            아니오
-          </button>
-        </div>
-        <button onClick={closeModal} className={styles["btn-close"]}>
-          <span className="sr-only">닫기</span>
+    <dialog ref={dialogRef} className={styles["stop-modal"]}>
+      <p>작성 중인 글이 있습니다. 창을 닫으시겠습니까?</p>
+      <div>
+        <button onClick={onConfirm} className={styles["btn-yes"]}>
+          네
         </button>
-      </dialog>
-    </>
+        <button onClick={closeModal} className={styles["btn-no"]}>
+          아니오
+        </button>
+      </div>
+      <button onClick={closeModal} className={styles["btn-close"]}>
+        <span className="sr-only">닫기</span>
+      </button>
+    </dialog>
   );
 }
