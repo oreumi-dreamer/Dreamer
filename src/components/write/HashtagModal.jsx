54b5fbfd6263f00858868086270de42dc1218085
han.j33 <jeehyun0212@naver.com>
2024-12-10 13:49:32 +0900

    ✨ 선택지 모달(HashtagModal) 리스트 불러오기, 선택개수 제한

--- src/components/write/HashtagModal.jsx
+++ src/components/write/HashtagModal.jsx
@@ -1,11 +1,8 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./HashtagModal.module.css";
+import { DREAM_GENRES, DREAM_MOODS } from "@/utils/constants";
 
-export default function HashtagModal({
-  isModalOpen,
-  closeModal,
-  selectedItems,
-}) {
+export default function HashtagModal({ isModalOpen, closeModal, onConfirm }) {
   const dialogRef = useRef(null);
   useEffect(() => {
     if (isModalOpen && dialogRef.current) {
@@ -17,6 +14,21 @@ export default function HashtagModal({
   const handleBackgroundClick = (e) => {
     if (e.target === e.currentTarget) closeModal();
   };
+  const [selectedGenres, setSelectedGenres] = useState([]);
+  const maxSelect = 5;
+
+  const handleCheckboxChange = (item) => {
+    if (selectedGenres.includes(item)) {
+      setSelectedGenres((prev) => prev.filter((i) => i !== item));
+    } else {
+      if (selectedGenres.length < maxSelect) {
+        setSelectedGenres((prev) => [...prev, item]);
+      }
+    }
+  };
+  const handleConfirm = () => {
+    onConfirm(selectedGenres);
+  };
 
   return (
     <dialog
@@ -26,15 +38,33 @@ export default function HashtagModal({
     >
       <form className={styles["hashtag-picker-container"]}>
         {/* 추후 선택지 데이터 불러오기 -- 시작 */}
-        <p>
-          <input type="checkbox" className={styles["hashtag-picker"]} id="1" />
-          <label for="1">선택지</label>
-        </p>
+        <ul>
+          {DREAM_GENRES.map((item) => (
+            <li key={item.id}>
+              <input
+                type="checkbox"
+                checked={selectedGenres.includes(item)}
+                onChange={() => handleCheckboxChange(item)}
+                className={styles["hashtag-picker"]}
+                id={item.id}
+              />
+              <label for={item.id}>{item.text}</label>
+            </li>
+          ))}
+          {/* <li>
+            <input
+              type="checkbox"
+              className={styles["hashtag-picker"]}
+              id="1"
+            />
+            <label for="1">선택지</label>
+          </li> */}
+        </ul>
         {/* 추후 선택지 데이터 불러오기 -- 끝 */}
       </form>
       <p className={styles["sub-text-container"]}>
         <span className={styles["sub-text"]}>(최대 5개 선택 가능)</span>
-        <button onClick={closeModal}>확인</button>
+        <button onClick={handleConfirm}>확인</button>
       </p>
     </dialog>
   );
