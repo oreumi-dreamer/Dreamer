996aadf1bd2328eb2723de725e03f0bfc6fc7e4a
chohee.hwang <manent556@gmail.com>
2024-12-10 21:22:53 +0900

    :art: [Fix] 모달포지션계산식 음수방지하는수식 추가

--- src/utils/calculateModalPosition.js
+++ src/utils/calculateModalPosition.js
@@ -1,11 +1,13 @@
 export const calculateModalPosition = (elementRef, offsetX = 0, offsetY = 0) => {
-    if (!elementRef?.current) return null;
-  
-    const elementRect = elementRef.current.getBoundingClientRect();
-    return {
-      position: "absolute",
-      top: `${elementRect.top + window.scrollY + offsetY}px`,
-      left: `${elementRect.left + window.scrollX + offsetX}px`,
-    };
+  if (!elementRef?.current) return null;
+
+  const rect = elementRef.current.getBoundingClientRect();
+  const left = Math.max(rect.left + window.scrollX + offsetX, 0);
+  const top = Math.max(rect.top + window.scrollY + offsetY, 0); 
+
+  return {
+    position: "absolute",
+    top: `${top}px`,
+    left: `${left}px`,
   };
-  
+};
