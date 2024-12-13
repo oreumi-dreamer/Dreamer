ed93a2a4287877964221e403a4b5f926552b9333
han.j33 <jeehyun0212@naver.com>
2024-12-13 13:38:31 +0900

    🐛 여백을 클릭해도 모달이 닫히는 문제 수정

--- src/components/write/WritePost.jsx
+++ src/components/write/WritePost.jsx
@@ -157,194 +157,199 @@ export default function WritePost({ isWriteModalOpen, closeWriteModal }) {
       open={isWritingModalOpen}
       onClick={handleBackgroundClick}
     >
-      <h2 className="sr-only">새로운 글 작성</h2>
-      <div className={styles["user-prof"]}>
-        <img src={profileImageUrl} width={52} height={52} />
-        <p className={styles["user-name"]}>
-          {userName}
-          <p className={styles["user-id"]}>@{userId}</p>
-        </p>
-      </div>
-
-      <button onClick={handleStopWriting} className={styles["btn-close"]}>
-        <Image src="/images/close.svg" width={40} height={40}></Image>
-        <span className="sr-only">닫기</span>
-      </button>
-
-      <form
-        id="new-post-form"
-        className={styles["new-post-form"]}
-        onSubmit={handleSubmit}
+      <div
+        className={styles["modal-contents"]}
+        onClick={(e) => e.stopPropagation()}
       >
-        <div id={styles["title"]}>
-          <label id={styles["title-input"]}>
-            Title
-            <input
-              type="text"
-              for="title"
-              placeholder={`${year}년 ${month}월 ${date}일 꿈 일기`}
-              onChange={handleTitleChange}
-            />
-          </label>
-          <label id={styles["hidden"]}>
-            <input
-              type="checkbox"
-              for="hidden"
-              checked={isPrivate}
-              onChange={() => setIsPrivate((prev) => !prev)}
-            />
-            비공개
-          </label>
-        </div>
-
-        <div className={styles["write-field"]}>
-          <div className={styles["write-field-opt"]}>
-            <div className={styles["genre-picker"]}>
-              <button type="button" onClick={openModal}>
-                <Image src="/images/plus-circle.svg" width={28} height={28} />
-                <span className="sr-only">태그 추가하기</span>
-              </button>
-              <ul>
-                {selectedGenres.map((item) => (
-                  <li
-                    key={item.text}
-                    style={
-                      theme === "light" ||
-                      (theme === "device" &&
-                        window.matchMedia("(prefers-color-scheme: light)")
-                          .matches)
-                        ? {
-                            backgroundColor: `${item.lightColor.hex}`,
-                            color:
-                              `${item.lightColor.textColor}` &&
-                              `${item.lightColor.textColor}`,
-                          }
-                        : {
-                            backgroundColor: `${item.darkColor.hex}`,
-                            color:
-                              `${item.darkColor.textColor}` &&
-                              `${item.darkColor.textColor}`,
-                          }
-                    }
-                  >
-                    {item.text}
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div className={styles["user-feeling"]}>
-              <p>지금 상태</p>
-              {selectedMoods.length === 0 && (
-                <button
-                  type="button"
-                  className={styles["btn-feeling"]}
-                  onClick={openMoodModal}
-                ></button>
-              )}
-              {selectedMoods.length !== 0 && (
-                <button
-                  type="button"
-                  className={styles["btn-feeling-selected"]}
-                  onClick={openMoodModal}
-                >
-                  <ul>
-                    {selectedMoods.map((item) => (
-                      <li key={item.text}>{item.text}</li>
-                    ))}
-                  </ul>
-                </button>
-              )}
-            </div>
-
-            <div className={styles["rate-star-container"]}>
-              <p>오늘의 꿈 별점: </p>
-              <input
-                type="radio"
-                className={styles["rate-star"]}
-                name="rate-star"
-                value={1}
-                onChange={handleRatingChange}
-                checked={rating === "1"}
-              />
-              <input
-                type="radio"
-                className={styles["rate-star"]}
-                name="rate-star"
-                value={2}
-                onChange={handleRatingChange}
-                checked={rating === "2"}
-              />
-              <input
-                type="radio"
-                className={styles["rate-star"]}
-                name="rate-star"
-                value={3}
-                onChange={handleRatingChange}
-                checked={rating === "3"}
-              />
-              <input
-                type="radio"
-                className={styles["rate-star"]}
-                name="rate-star"
-                value={4}
-                onChange={handleRatingChange}
-                checked={rating === "4"}
-              />
-              <input
-                type="radio"
-                className={styles["rate-star"]}
-                name="rate-star"
-                value={5}
-                onChange={handleRatingChange}
-                checked={rating === "5"}
-              />
-            </div>
-          </div>
-          <span className={styles["break-line"]}></span>
-          <p className={styles["text-field-area"]}>
-            <span className="sr-only">글 작성</span>
-            <textarea
-              placeholder="오늘은 어떤 꿈을 꾸셨나요?"
-              className={styles["text-field-area"]}
-              onChange={handleContentChange}
-            />
+        <h2 className="sr-only">새로운 글 작성</h2>
+        <div className={styles["user-prof"]}>
+          <img src={profileImageUrl} width={52} height={52} />
+          <p className={styles["user-name"]}>
+            {userName}
+            <p className={styles["user-id"]}>@{userId}</p>
           </p>
         </div>
-        <div className={styles["btn-submit-area"]}>
-          <button
-            type="submit"
-            form="new-post-form"
-            className={styles["btn-submit"]}
-          >
-            전송
-          </button>
-        </div>
-      </form>
 
-      <HashtagModal
-        isModalOpen={isModalOpen}
-        closeModal={closeModal}
-        onConfirm={handleSelectGenres}
-      />
-      <MoodModal
-        isModalOpen={isMoodModalOpen}
-        closeModal={closeMoodModal}
-        onConfirm={handleSelectMoods}
-      />
-      {isStopModalOpen && (
-        <StopModal
-          isStopModalOpen={isStopModalOpen}
-          closeModal={closeStopModal}
-          onConfirm={() => {
-            closeWriteModal();
-            setInputValue("");
-            setContentValue("");
-            setSelectedGenres([]);
-            setSelectedMoods([]);
-            setIsContentChanged(false);
-          }}
+        <button onClick={handleStopWriting} className={styles["btn-close"]}>
+          <Image src="/images/close.svg" width={40} height={40}></Image>
+          <span className="sr-only">닫기</span>
+        </button>
+
+        <form
+          id="new-post-form"
+          className={styles["new-post-form"]}
+          onSubmit={handleSubmit}
+        >
+          <div id={styles["title"]}>
+            <label id={styles["title-input"]}>
+              Title
+              <input
+                type="text"
+                for="title"
+                placeholder={`${year}년 ${month}월 ${date}일 꿈 일기`}
+                onChange={handleTitleChange}
+              />
+            </label>
+            <label id={styles["hidden"]}>
+              <input
+                type="checkbox"
+                for="hidden"
+                checked={isPrivate}
+                onChange={() => setIsPrivate((prev) => !prev)}
+              />
+              비공개
+            </label>
+          </div>
+
+          <div className={styles["write-field"]}>
+            <div className={styles["write-field-opt"]}>
+              <div className={styles["genre-picker"]}>
+                <button type="button" onClick={openModal}>
+                  <Image src="/images/plus-circle.svg" width={28} height={28} />
+                  <span className="sr-only">태그 추가하기</span>
+                </button>
+                <ul>
+                  {selectedGenres.map((item) => (
+                    <li
+                      key={item.text}
+                      style={
+                        theme === "light" ||
+                        (theme === "device" &&
+                          window.matchMedia("(prefers-color-scheme: light)")
+                            .matches)
+                          ? {
+                              backgroundColor: `${item.lightColor.hex}`,
+                              color:
+                                `${item.lightColor.textColor}` &&
+                                `${item.lightColor.textColor}`,
+                            }
+                          : {
+                              backgroundColor: `${item.darkColor.hex}`,
+                              color:
+                                `${item.darkColor.textColor}` &&
+                                `${item.darkColor.textColor}`,
+                            }
+                      }
+                    >
+                      {item.text}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className={styles["user-feeling"]}>
+                <p>지금 상태</p>
+                {selectedMoods.length === 0 && (
+                  <button
+                    type="button"
+                    className={styles["btn-feeling"]}
+                    onClick={openMoodModal}
+                  ></button>
+                )}
+                {selectedMoods.length !== 0 && (
+                  <button
+                    type="button"
+                    className={styles["btn-feeling-selected"]}
+                    onClick={openMoodModal}
+                  >
+                    <ul>
+                      {selectedMoods.map((item) => (
+                        <li key={item.text}>{item.text}</li>
+                      ))}
+                    </ul>
+                  </button>
+                )}
+              </div>
+
+              <div className={styles["rate-star-container"]}>
+                <p>오늘의 꿈 별점: </p>
+                <input
+                  type="radio"
+                  className={styles["rate-star"]}
+                  name="rate-star"
+                  value={1}
+                  onChange={handleRatingChange}
+                  checked={rating === "1"}
+                />
+                <input
+                  type="radio"
+                  className={styles["rate-star"]}
+                  name="rate-star"
+                  value={2}
+                  onChange={handleRatingChange}
+                  checked={rating === "2"}
+                />
+                <input
+                  type="radio"
+                  className={styles["rate-star"]}
+                  name="rate-star"
+                  value={3}
+                  onChange={handleRatingChange}
+                  checked={rating === "3"}
+                />
+                <input
+                  type="radio"
+                  className={styles["rate-star"]}
+                  name="rate-star"
+                  value={4}
+                  onChange={handleRatingChange}
+                  checked={rating === "4"}
+                />
+                <input
+                  type="radio"
+                  className={styles["rate-star"]}
+                  name="rate-star"
+                  value={5}
+                  onChange={handleRatingChange}
+                  checked={rating === "5"}
+                />
+              </div>
+            </div>
+            <span className={styles["break-line"]}></span>
+            <p className={styles["text-field-area"]}>
+              <span className="sr-only">글 작성</span>
+              <textarea
+                placeholder="오늘은 어떤 꿈을 꾸셨나요?"
+                className={styles["text-field-area"]}
+                onChange={handleContentChange}
+              />
+            </p>
+          </div>
+          <div className={styles["btn-submit-area"]}>
+            <button
+              type="submit"
+              form="new-post-form"
+              className={styles["btn-submit"]}
+            >
+              전송
+            </button>
+          </div>
+        </form>
+
+        <HashtagModal
+          isModalOpen={isModalOpen}
+          closeModal={closeModal}
+          onConfirm={handleSelectGenres}
         />
-      )}
+        <MoodModal
+          isModalOpen={isMoodModalOpen}
+          closeModal={closeMoodModal}
+          onConfirm={handleSelectMoods}
+        />
+        {isStopModalOpen && (
+          <StopModal
+            isStopModalOpen={isStopModalOpen}
+            closeModal={closeStopModal}
+            onConfirm={() => {
+              closeWriteModal();
+              setInputValue("");
+              setContentValue("");
+              setSelectedGenres([]);
+              setSelectedMoods([]);
+              setIsContentChanged(false);
+            }}
+          />
+        )}
+      </div>
     </dialog>
   );
 }
