21dcc97786334697d2c662dc6e4556f5aea3032b
김지훈 <kim@jihun.io>
2024-12-15 03:29:38 +0900

    ♻️ 불필요한 헤더와 푸터 컴포넌트를 제거하고 페이지 구조 간소화

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,8 @@
 import { useSelector } from "react-redux";
 import styles from "./page.module.css";
 import SocialLogin from "@/components/login/SocialLogin";
-import Header from "@/components/header/Header";
-import Image from "next/image";
 import Link from "next/link";
-import PostModal from "@/components/modal/PostModal";
-import Footer from "@/components/footer/Footer";
-import Profile from "@/components/profile/Profile";
 import MainList from "@/components/main/MainList";
-import { CustomScrollbar } from "@/components/Controls";
 
 export default function Home() {
   const { user, isRegistrationComplete } = useSelector((state) => state.auth);
@@ -34,12 +28,5 @@ export default function Home() {
     return null;
   }
 
-  return (
-    <div id="container" className={styles.container}>
-      <Header />
-      <MainList />
-      <Footer />
-      <CustomScrollbar />
-    </div>
-  );
+  return <MainList />;
 }
