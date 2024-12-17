a0b1be966587b798cf464a6d204fbd8e70fef7e1
김지훈 <kim@jihun.io>
2024-12-17 10:24:33 +0900

    💄 테마 핸들러 컴포넌트 추가: 사용자 테마 변경 감지 및 적용 기능 구현

---
+++ src/components/theme/ThemeHandler.jsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function ThemeHandler({ children }) {
+  useEffect(() => {
+    // 미디어 쿼리 매칭 상태를 감지하는 함수
+    const handleThemeChange = (e) => {
+      const userTheme = localStorage.getItem("userTheme");
+      const html = document.querySelector("html");
+
+      if (userTheme === "device") {
+        html.dataset.theme = e.matches ? "dark" : "light";
+      }
+    };
+
+    // 미디어 쿼리 매칭 객체 생성
+    const darkModeMediaQuery = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    );
+
+    // 초기 테마 설정
+    handleThemeChange(darkModeMediaQuery);
+
+    // 시스템 테마 변경 이벤트 리스너 등록
+    darkModeMediaQuery.addEventListener("change", handleThemeChange);
+
+    // 컴포넌트 언마운트 시 이벤트 리스너 제거
+    return () => {
+      darkModeMediaQuery.removeEventListener("change", handleThemeChange);
+    };
+  }, []); // deviceTheme 의존성 제거
+
+  return <>{children}</>;
+}
