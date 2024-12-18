f78b442cc55e336bf537ece1049f36167800576d
김지훈 <kim@jihun.io>
2024-12-18 20:00:36 +0900

    🐛 모달 표시 조건을 추가하여 불필요한 렌더링 방지 및 글쓰기 모달 간 충돌 수정

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -209,10 +209,12 @@ export default function Profile({ userName, write }) {
         isShow={isShowModal}
         onClose={handleModalClose}
       />
-      <WritePost
-        isWriteModalOpen={isWriteModalOpen}
-        closeWriteModal={closeWriteModal}
-      />
+      {isWriteModalOpen && (
+        <WritePost
+          isWriteModalOpen={isWriteModalOpen}
+          closeWriteModal={closeWriteModal}
+        />
+      )}
       {isLoginModalOpen && (
         <ConfirmModal
           message="로그인이 필요합니다. 로그인 하시겠습니까?"
