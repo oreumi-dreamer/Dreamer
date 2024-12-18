fc741d16ed3ce6b6660af4aab1bfcb1000afd1a7
han.j33 <jeehyun0212@naver.com>
2024-12-18 16:21:42 +0900

    📝 업로드중 마크업 생성

---
+++ src/components/write/Uploading.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Uploading.module.css";
+
+export default function Uploading() {
+  return (
+    <div className={styles["container"]}>
+      <div className={styles["line"]}></div>
+    </div>
+  );
+}
