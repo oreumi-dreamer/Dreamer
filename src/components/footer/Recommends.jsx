59a4dd7858677c09c8249936d2111199c5408a93
김지훈 <kim@jihun.io>
2024-12-22 13:46:35 +0900

    🧑‍💻 프로필에서 팔로잉 카운트가 즉각 반영되도록 Redux로 상태 관리

--- src/components/footer/Recommends.jsx
+++ src/components/footer/Recommends.jsx
@@ -5,10 +5,15 @@ import Loading from "../Loading";
 import { fetchWithAuth } from "@/utils/auth/tokenUtils";
 import Link from "next/link";
 import { Button } from "../Controls";
+import { useDispatch, useSelector } from "react-redux";
+import { loginSuccess } from "@/store/authSlice";
 
 export default function Recommends({ className, styles }) {
   const [usersList, setUsersList] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.auth.user);
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -22,10 +27,12 @@ export default function Recommends({ className, styles }) {
     fetchUsers();
   }, []);
 
-  const handleFollow = async (user) => {
+  const handleFollow = async (targetUser) => {
+    const isFollowingBool = targetUser.isFollowing;
+
     setUsersList((prev) =>
       prev.map((u) => {
-        if (u.userId === user.userId) {
+        if (u.userId === targetUser.userId) {
           return {
             ...u,
             isFollowing: !u.isFollowing,
@@ -35,18 +42,42 @@ export default function Recommends({ className, styles }) {
       })
     );
 
-    const response = await fetchWithAuth(`/api/account/follow/${user.userId}`);
+    dispatch(
+      loginSuccess({
+        user: {
+          ...user,
+          followingCount: isFollowingBool
+            ? user.followingCount - 1
+            : user.followingCount + 1,
+        },
+      })
+    );
+
+    const response = await fetchWithAuth(
+      `/api/account/follow/${targetUser.userId}`
+    );
 
     if (!response.ok) {
       setUsersList((prev) =>
         prev.map((u) => {
-          if (u.userId === user.userId) {
+          if (u.userId === targetUser.userId) {
             return {
               ...u,
               isFollowing: !u.isFollowing,
             };
           }
           return u;
+        })
+      );
+
+      dispatch(
+        loginSuccess({
+          user: {
+            ...user,
+            followingCount: isFollowingBool
+              ? user.followingCount - 1
+              : user.followingCount + 1,
+          },
         })
       );
     }
